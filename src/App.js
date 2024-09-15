a50dc15f41e00df53bedc1d3ca0df2deebde9ffc
KenJohn28 <johnsonmok1129@gmail.com>
2024-09-15 18:47:08 -0400

    Added in hyperlinks that redirects to my github account and netlify webpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import WeatherSearch from './WeatherSearch';
+import "./App.css";
+import WeatherSearch from "./WeatherSearch";
 
 function App() {
   return (
@@ -8,6 +8,16 @@ function App() {
         <h1>Weather App</h1>
         <WeatherSearch />
       </header>
+      <p className="Links">
+        This Project was coded by <a href="blank">Kenya Johnson</a> and{" "}
+        <a href="https://github.com/KenJohn28/Weatther-Search-App">
+          is open-sourced on Github{" "}
+        </a>{" "}
+        and{" "}
+        <a href="https://kjs-weather-search-app.netlify.app/">
+          hosted on Netlify
+        </a>
+      </p>
     </div>
   );
 }
